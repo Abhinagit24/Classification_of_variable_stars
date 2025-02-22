c951a139631bb6f7aa0e3a8ccfacf6435f963fe6
Abhina Premachandran <76970901+Abhinagit24@users.noreply.github.com>
2025-02-22 13:18:36 -0500

    updated the files

--- NN_train_negative_data.py
+++ NN_train_negative_data.py
@@ -9,13 +9,13 @@ from getting_dataset1 import get_train_dataset, get_validation_dataset
 def main():
     train_light_curve_dataset = get_train_dataset()
     validation_light_curve_dataset = get_validation_dataset()
-    model = HadryssNew.new(end_module=HadryssMultiClassScoreEndModuleNew(number_of_classes=6))
+    model = HadryssNew.new(end_module=HadryssMultiClassScoreEndModuleNew(number_of_classes=5))
     optimizer = AdamW(model.parameters(), lr=1e-3)
     train_hyperparameter_configuration = TrainHyperparameterConfiguration.new(
         batch_size=1000, cycles=1000, train_steps_per_cycle=100, validation_steps_per_cycle=50)
     loss_function = CrossEntropyAlt()
-    metric_functions = [CrossEntropyAlt(), MulticlassAccuracyAlt(number_of_classes=6),
-                        MulticlassAUROCAlt(number_of_classes=6)]
+    metric_functions = [CrossEntropyAlt(), MulticlassAccuracyAlt(number_of_classes=5),
+                        MulticlassAUROCAlt(number_of_classes=5)]
     train_session(train_datasets=[train_light_curve_dataset], validation_datasets=[validation_light_curve_dataset],
                   model=model, optimizer=optimizer, loss_function=loss_function, metric_functions=metric_functions,
                   hyperparameter_configuration=train_hyperparameter_configuration)
